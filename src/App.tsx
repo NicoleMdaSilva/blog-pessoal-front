0e5cacd90d5f1adfed8fd4f37e9bbee967aeb72b
Name <email@host.com>
2022-11-29 16:32:23 -0300

    feat: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
-import { Title } from './components/Title/Title';
-import { Text } from './components/Text/Text';
+import { Footer } from './components/estaticos/Footer/Footer';
+import NavBar from './components/estaticos/NavBar/NavBar';
+import { About } from './components/Paginas/About/About';
+import { Home } from './components/Paginas/Home/Home';
 
 function App() {
-  const nome = 'Nicole';
-
   return(
     <>
-      <Title nome={nome} />
-      <Text />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} /> 
+        <Route path='/home' element={<Home />} /> 
+        <Route path='/about' element={<About />} /> 
+        </Routes>
+      <Footer />
+    </Router>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Checkbox, createTheme, ThemeProvider} from '@material-ui/core';
+
 
 //o root do index.html e onde vamos armazenar nosso sistema
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#e91e63',
+    },
+    secondary: {
+      main: '#aa00ff',
+    },
+  },
+});
 
 //mandando renderizar o código na div root
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
