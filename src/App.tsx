dab0103918501331941d34d1c1debf7a545e4ef4
NicoleMdaSilva <pauloskx9004@gmail.com>
2022-12-07 10:48:32 -0300

    feat: login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import './App.css';
 import { Footer } from './components/estaticos/Footer/Footer';
 import NavBar from './components/estaticos/NavBar/NavBar';
 import { About } from './components/Paginas/About/About';
 import { Home } from './components/Paginas/Home/Home';
+import { Login } from './components/Paginas/login/login';
+import { store } from './store/store';
 
 function App() {
-  return(
+  return (
     <>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} /> 
-        <Route path='/home' element={<Home />} /> 
-        <Route path='/about' element={<About />} /> 
-        </Routes>
-      <Footer />
-    </Router>
+      <Provider store={store}>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<Navigate to='/' replace />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </Provider>
     </>
   );
 }
